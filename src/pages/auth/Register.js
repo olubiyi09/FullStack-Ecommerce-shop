82d510a77ef3caeec738835d6deb3137c7a5c5b4
olubiyi09 <olubiyi09@gmail.com>
2022-12-01 02:12:43 +0400

    created the files and components for the project

---
+++ src/pages/auth/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./auth.module.scss";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/pages/auth/Reset.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./auth.module.scss";
+
+const Reset = () => {
+  return (
+    <div>
+      <h1>Reset</h1>
+    </div>
+  );
+};
+
+export default Reset;
