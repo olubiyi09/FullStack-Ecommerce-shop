f90889b27b11c2f4bfe6dcea7cce553483cde0b5
olubiyi09 <olubiyi09@gmail.com>
2022-12-01 03:06:58 +0400

    completed the JSX for the register and reset password page

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,11 +1,38 @@
 import React from "react";
 import styles from "./auth.module.scss";
+import RegisterImg from "../../assets/register.png";
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <section className={`container ${styles.auth} `}>
+      <Card>
+        <div className={styles.form}>
+          <h2>Register</h2>
+
+          <form>
+            <input type="text" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <input type="password" placeholder="Confirm Password" required />
+            <button className="--btn --btn-primary --btn-block">
+              Register
+            </button>
+
+            <div className={styles.links}></div>
+          </form>
+
+          <span className={styles.register}>
+            <p>Already have an account?</p>
+            <Link to="/login"> Login</Link>
+          </span>
+        </div>
+      </Card>
+
+      <div className={styles.img}>
+        <img src={RegisterImg} alt="Register-pic" width={400} />
+      </div>
+    </section>
   );
 };
 
--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,11 +1,36 @@
-import React from "react";
 import styles from "./auth.module.scss";
+import ResetImg from "../../assets/forgot.png";
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
 
 const Reset = () => {
   return (
-    <div>
-      <h1>Reset</h1>
-    </div>
+    <section className={`container ${styles.auth} `}>
+      <div className={styles.img}>
+        <img src={ResetImg} alt="Reset-img" width={400} />
+      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Reset Password</h2>
+
+          <form>
+            <input type="text" placeholder="Enter Email" required />
+            <button className="--btn --btn-primary --btn-block">
+              Reset Password
+            </button>
+
+            <div className={styles.links}>
+              <p>
+                <Link to="/login">- Login</Link>
+              </p>
+              <p>
+                <Link to="/register">- Register</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </section>
   );
 };
 
